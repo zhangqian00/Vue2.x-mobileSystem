31e128148e6c71225fdbf66140921aa624dcac3c
zhangqian <zq_674360351@163.com>
2018-01-08 21:44:34 +0800

    添加字体

--- components/app.vue
+++ components/app.vue
@@ -14,8 +14,8 @@
 				<span class="mui-tab-label">消息</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-contact">
-				<span class="mui-icon mui-icon-contact"></span>
-				<span class="mui-tab-label">通讯录</span>
+				<span class="mui-icon icon-gouwuchekong"></span>
+				<span class="mui-tab-label">购物车</span>
 			</a>
 			<a class="mui-tab-item" href="#tabbar-with-map">
 				<span class="mui-icon mui-icon-gear"></span>
